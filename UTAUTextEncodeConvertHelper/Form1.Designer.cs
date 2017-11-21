d0aae470e9fd2993b7ae1eeb12285a7c0f59cd3e
Milkey Tan <819774989@qq.com>
2017-11-21 09:53:39 +0800

    ver

--- UTAUTextEncodeConvertHelper/Form1.Designer.cs
+++ UTAUTextEncodeConvertHelper/Form1.Designer.cs
@@ -337,7 +337,7 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "UTAU TextEncode Convert Helper 0.56";
+            this.Text = "UTAU TextEncode Convert Helper 1.21";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
