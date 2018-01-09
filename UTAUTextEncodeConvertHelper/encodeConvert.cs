abcb8e6646697f7d8d38693d56d6327ba5ce1a95
Milkey Tan <819774989@qq.com>
2018-01-09 11:20:38 +0800

    Sty

--- UTAUTextEncodeConvertHelper/encodeConvert.cs
+++ UTAUTextEncodeConvertHelper/encodeConvert.cs
@@ -6,17 +6,17 @@ namespace UTAUTextEncodeConvertHelper
     {
         public static string Converter(string str,Encoding encode)
         {
-            if (encode == Encoding.GetEncoding("Shift_JIS"))
+            if (Equals(encode, Encoding.GetEncoding("Shift_JIS")))
             {
                 string strAfterConvert = Encoding.GetEncoding("gb2312").GetString(Encoding.GetEncoding("Shift_JIS").GetBytes(str));
                 return strAfterConvert;
             }
-            else if (encode == Encoding.GetEncoding("gb2312"))
+            else if (Equals(encode, Encoding.GetEncoding("gb2312")))
             {
                 string strAfterConvert = Encoding.GetEncoding("Shift_JIS").GetString(Encoding.GetEncoding("gb2312").GetBytes(str));
                 return strAfterConvert;
             }
-            else if (encode == Encoding.UTF8)
+            else if (Equals(encode, Encoding.UTF8))
             {
                 string strAfterConvert = Encoding.UTF8.GetString(Encoding.GetEncoding("gb2312").GetBytes(str));
                 return strAfterConvert;
